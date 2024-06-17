cf8907e6a6754454f253045df86cabdc5eeeea6e
mathew-tran <mathew.tran.dev@gmail.com>
2024-06-17 08:52:18 -0700

    add an explosion when delivering item

--- Scripts/DropArea.gd
+++ Scripts/DropArea.gd
@@ -3,6 +3,9 @@ extends Area2D
 
 func _on_body_entered(body):
 	if body.is_in_group("Box"):
+		var explosion = load("res://Prefabs/Effects/DropExplosion.tscn").instantiate()
+		explosion.global_position = global_position
+		EventManager.GetItemsGroup().add_child(explosion)
 		EventManager.GiveReward()
 		body.queue_free()
 		EventManager.ClearDestination()
---
+++ Scripts/Effects/DropExplosion.gd
@@ -0,0 +1,10 @@
+extends CPUParticles2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	emitting = true
+
+func _process(delta):
+	if emitting == false:
+		queue_free()
